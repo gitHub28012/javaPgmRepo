220491c316707af5f3eafe8645f14b7c38b0a209
Tanmay <github2801@gmail.com>
2024-07-01 12:58:25 +0530

    Second Commit

---
+++ src/qsp/DemoClass.java
@@ -0,0 +1,9 @@
+package qsp;
+
+public class DemoClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Pusing Demo class from git to gitHub");
+	}
+
+}
--- src/qsp/Docter.java
+++ src/qsp/Docter.java
@@ -1,9 +1,9 @@
-// this is Docter . java file added from gitHub Repo
+package qsp;
 public class Docter{
 
    public static void main(String [] args)
   {
-
+	// this is Docter . java file added from gitHub Repo
     System.out.println("Docter added from gitHubREPO");
 
   }
