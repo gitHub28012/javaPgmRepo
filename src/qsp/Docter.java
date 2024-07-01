6dba5a2b41206cab9c1d54f7fdc8b495f90fda78
GitHub28012 <123802236+gitHub28012@users.noreply.github.com>
2024-07-01 12:53:18 +0530

    Create Docter.java
    New file added from Github Repository

---
+++ src/qsp/Docter.java
@@ -0,0 +1,13 @@
+// this is Docter . java file added from gitHub Repo
+public class Docter{
+
+   public static void main(String [] args)
+  {
+
+    System.out.println("Docter added from gitHubREPO");
+
+  }
+
+
+
+}
